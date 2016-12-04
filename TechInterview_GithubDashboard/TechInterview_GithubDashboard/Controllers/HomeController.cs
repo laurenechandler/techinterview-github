e20524a4d7d2fe2a6ee8529526f42c0baed70067
Lauren Chandler <laurenechandler16@gmail.com>
2016-12-03 23:47:42 -0600

    Updated code to call Github API and parsed response into TestModel

--- TechInterview_GithubDashboard/TechInterview_GithubDashboard/Controllers/HomeController.cs
+++ TechInterview_GithubDashboard/TechInterview_GithubDashboard/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TechInterview_GithubDashboard.Models;
 
 namespace TechInterview_GithubDashboard.Controllers
 {
@@ -10,7 +14,19 @@ namespace TechInterview_GithubDashboard.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //TestModel testModel = new TestModel();
+            //testModel.ID = 45345;
+            //testModel.Name = "Lauren";
+
+            string url = "https://api.github.com/events";
+            HttpWebRequest webRequestURL = (HttpWebRequest)WebRequest.Create(url);
+            webRequestURL.UserAgent = "application/vnd.github.v3+json";
+            Stream dataStream = webRequestURL.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            var temp = JsonConvert.DeserializeObject<IEnumerable<TestModel>>(reader.ReadToEnd());
+
+
+            return View(temp);
         }
 
         public ActionResult About() 
---
+++ TechInterview_GithubDashboard/TechInterview_GithubDashboard/Models/TestModel.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechInterview_GithubDashboard.Models
+{
+    public class TestModel
+    {
+        private string id;
+        private string type;
+        private string created;
+
+        [JsonProperty("id")]
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [JsonProperty("type")]
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        [JsonProperty("created_at")]
+        public string Created
+        {
+            get { return created; }
+            set { created = value; }
+        }
+    }
+}
